09b0a8571ee1892600b8bad9449bc61d6674c7a5
Andrew Buescher <andrew.buescher99@gmail.com>
2026-01-12 18:01:58 -0500

    Initial Commit

---
+++ gym app/gym_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  gym_appApp.swift
+//  gym app
+//
+//  Created by  Andrew Buescher on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct gym_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
